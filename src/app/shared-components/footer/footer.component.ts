6a0e61f159ef88b5f2ac8a67d14f9bbcd61b9081
Ekalape <ekalape@outlook.it>
2023-12-29 08:54:39 +0100

    feat: implement footer

--- src/app/shared-components/footer/footer.component.ts
+++ src/app/shared-components/footer/footer.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.scss'],
+  standalone: true,
 })
 export class FooterComponent implements OnInit {
 
@@ -13,3 +14,31 @@ export class FooterComponent implements OnInit {
   }
 
 }
+
+
+/* import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appCardHover]'
+})
+export class CardHoverDirective {
+
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.scaleImage(1.2); // Scale factor for mouse enter
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.scaleImage(1); // Reset to original size on mouse leave
+  }
+
+  private scaleImage(scale: number) {
+    const imageElement = this.el.nativeElement.querySelector('.card-image'); // Adjust the selector based on your HTML structure
+    if (imageElement) {
+      this.renderer.setStyle(imageElement, 'transform', `scale(${scale})`);
+      this.renderer.setStyle(imageElement, 'transition', 'transform 0.3s ease'); // Add a smooth transition
+    }
+  }
+}
+ */
