2e8ca2c3a93cd33c255ad77c3ad84d37c1ec8813
Ekalape <ekalape@outlook.it>
2024-01-05 16:24:36 +0100

    feat: implement app section for home-page, add responsive menu in the header with animated drawer and open-close btn

--- src/app/shared-components/header/header.component.ts
+++ src/app/shared-components/header/header.component.ts
@@ -1,22 +1,62 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, HostBinding } from '@angular/core';
+import { CommonModule, ViewportScroller } from '@angular/common';
 import { FirstWordPipe } from '../pipes/first-word.pipe';
 import { RouterModule } from '@angular/router';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [CommonModule, FirstWordPipe, RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  animations: [
+    trigger('drawerAnimation', [
+      state('close', style({
+        right: '-100%'
+      })),
+      state('open', style({
+        right: '0px'
+      })),
+      transition('close=>open', [
+        animate('500ms ease-out', style({
+          right: '0px'
+        }))
+      ]),
+      transition('open=>close', [
+        animate('500ms ease-out', style({
+          right: '-100%'
+        }))
+      ]),
+    ])
+  ]
 })
 export class HeaderComponent {
+
+  opened = false;
 
   titles: string[] = ["Favorite coffee", "About", "Mobile app", "Contact us"]
   selectedTitle: string = "Favorite coffee"
 
+
+  constructor(private viewportScroller: ViewportScroller) { }
+
+  scrollToComponent(componentId: string, title: string): void {
+    const element = document.getElementById(componentId);
+
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+    this.selectHeaderMenuTitle(title)
+    if (this.opened) this.opened = false;
+  }
+
   selectHeaderMenuTitle(title: string) {
     this.selectedTitle = title;
+  }
+
+  openCloseDrawer() {
+    this.opened = !this.opened
   }
 
 }
